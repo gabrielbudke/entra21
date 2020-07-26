4afc23b0ab0c7eabecfe99246c579f1b1c7a39ce
Gabriel Sousa <gabrielbudke@gmail.com>
2020-07-26 18:11:20 -0300

    Adicionando exercicios da lista 1

--- src/br/com/entra21/exercicios/lista1/Exercicio09.java
+++ src/br/com/entra21/exercicios/lista1/Exercicio09.java
@@ -1,5 +1,26 @@
 package br.com.entra21.exercicios.lista1;
 
+import javax.swing.JOptionPane;
+
+/**
+ * 9. Solicite dois números e inverta os valores destas variáveis.
+ */
+/**
+ * @author Gabriel B Sousa
+ */
 public class Exercicio09 {
-    
+    public static void main(String[] args) {
+        
+        int aux, numero_1, numero_2;
+        
+        numero_1 = Integer.parseInt(JOptionPane.showInputDialog("Número 1:"));
+        numero_2 = Integer.parseInt(JOptionPane.showInputDialog("Número 2:"));
+
+        aux = numero_1;
+        numero_1 = numero_2;
+        numero_2 = aux;
+
+        JOptionPane.showMessageDialog(null, "Número 1: " + numero_1 + "\nNúmero 2: " + numero_2);
+
+    }
 }
--- src/br/com/entra21/exercicios/lista1/Exercicio10.java
+++ src/br/com/entra21/exercicios/lista1/Exercicio10.java
@@ -1,5 +1,21 @@
 package br.com.entra21.exercicios.lista1;
 
+import javax.swing.JOptionPane;
+
+/**
+ * 10. Solicite o ano de nascimento do usuário e apresente ao final a idade do mesmo.
+ */
+/**
+ * @author Gabriel B Sousa
+ */
 public class Exercicio10 {
-    
+    public static void main(String[] args) {
+        
+        int anoNascimento = Integer.parseInt(JOptionPane.showInputDialog("Ano de nascimento:"));
+
+        int idadeAtual = 2018 - anoNascimento;
+
+        JOptionPane.showMessageDialog(null, "A sua idade em 2018 é: " + idadeAtual);
+
+    }
 }
