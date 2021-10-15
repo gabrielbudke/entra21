416fceb6152b60c5e36604f772e97729e978d9c4
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-10-15 03:30:44 +0000

    :recycle: Exercitando lógica de programação

--- src/br/com/entra21/exercicios/lista02/Exercicio18.java
+++ src/br/com/entra21/exercicios/lista02/Exercicio18.java
@@ -11,56 +11,152 @@ import java.util.Scanner;
 public class Exercicio18 {
    public static void main(String[] args) {
 
+      String horasNascimentoPorExtenso;
       Scanner scan = new Scanner(System.in);
 
-      System.out.print("Informe a hora de nascimento (HH:MM:SS): ");
+      System.out.print(
+            "==== ALGORITMO PARA ESCREVER AS HORAS POR EXTENSO ====" + "\nInforme a hora de nascimento (HH:MM:SS): ");
       String horasNascimento = scan.nextLine();
 
       if (horasNascimento.length() == 8) {
-         System.out.println(horasPorExtenso(horasNascimento));
+
+         String horas = horasNascimento.substring(0, 2);
+         String minutos = horasNascimento.substring(3, 5);
+         String segundos = horasNascimento.substring(6, 8);
+
+         String horasPorExtenso = horasPorExtenso(horas);
+         String minutosPorExtenso = minutosPorExtenso(minutos);
+         String segundosPorExtenso = segundosPorExtenso(segundos);
+
+         if (minutosPorExtenso.length() > 0 && segundosPorExtenso.length() > 0) {
+            horasNascimentoPorExtenso = "A horas do nascimento é: " + horasPorExtenso + ", " + minutosPorExtenso + " e "
+                  + segundosPorExtenso;
+         } else if (minutosPorExtenso.length() > 0 && segundosPorExtenso.length() == 0) {
+            horasNascimentoPorExtenso = "A horas do nascimento é: " + horasPorExtenso + " e " + minutosPorExtenso;
+         } else if (minutosPorExtenso.length() == 0 && segundosPorExtenso.length() > 0) {
+            horasNascimentoPorExtenso = "A horas do nascimento é: " + horasPorExtenso + " e " + segundosPorExtenso;
+         } else {
+            horasNascimentoPorExtenso = "As horas do nascimento são: " + horasPorExtenso;
+         }
+
+         System.out.println(horasNascimentoPorExtenso);
       } else {
          System.out.println("O formato das horas é inválido!");
       }
 
    }
 
-   public static String horasPorExtenso(String horasNascimento) {
+   public static String horasPorExtenso(String horas) {
 
       String horasPorExtenso = "";
 
       String[] arrayHoras = { "meia-noite", "uma", "duas", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",
-            "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove",
+            "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove",
             "vinte", "vinte e uma", "vinte e duas", "vinte e três", "meia-noite" };
 
-      String horas = horasNascimento.substring(0, 2);
-      String minutos = horasNascimento.substring(3, 5);
-      String segundos = horasNascimento.substring(6, 8);
-
       int horasConvertidasNumero = Integer.parseInt(horas);
-      int minutosConvertidosNumero = Integer.parseInt(minutos);
-      int segundosConvertidosNumero = Integer.parseInt(segundos);
 
       if (horasConvertidasNumero > 24) {
          throw new Error("As horas são inválidas!");
       }
-
-      if (minutosConvertidosNumero >= 60) {
-         throw new Error("Os minutos são inválidos!");
-      }
-
-      if (segundosConvertidosNumero >= 60) {
-         throw new Error("Os segundos são inválidos!");
-      }
-
+      /*
+       * 
+       * 
+       * if (segundosConvertidosNumero >= 60) { throw new
+       * Error("Os segundos são inválidos!"); }
+       */
       if (horasConvertidasNumero > 1 && horasConvertidasNumero < 24) {
          horasPorExtenso += arrayHoras[horasConvertidasNumero] + " horas";
       } else if (horasConvertidasNumero == 1) {
          horasPorExtenso += arrayHoras[horasConvertidasNumero] + " hora";
       } else {
-         horasPorExtenso += arrayHoras[horasConvertidasNumero] + " hora";
+         horasPorExtenso += arrayHoras[horasConvertidasNumero];
       }
 
       return horasPorExtenso;
    }
 
+   public static String minutosPorExtenso(String minutos) {
+
+      String minutosPorExtenso = "";
+
+      String[] dezena = { "", "", "vinte", "trinta", "quarenta", "cinquenta" };
+      String[] unidade = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze",
+            "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+
+      int minutosConvertidosNumero = Integer.parseInt(minutos);
+
+      if (minutosConvertidosNumero >= 60) {
+         throw new Error("Os minutos são inválidos!");
+      }
+
+      if (minutosConvertidosNumero == 0) {
+         return "";
+      }
+
+      if (minutosConvertidosNumero == 1) {
+         minutosPorExtenso = unidade[minutosConvertidosNumero] + " minuto";
+      } else if (minutosConvertidosNumero < 20) {
+         minutosPorExtenso = unidade[minutosConvertidosNumero] + " minutos";
+      } else {
+         String minutoDezena = minutos.substring(0, 1);
+         String minutoUnidade = minutos.substring(1, 2);
+
+         int minutoDezenaConvertidoNumero = Integer.parseInt(minutoDezena);
+         int minutoUnidadeConvertidoNumero = Integer.parseInt(minutoUnidade);
+
+         if (minutoUnidadeConvertidoNumero == 0) {
+            minutosPorExtenso = dezena[minutoDezenaConvertidoNumero] + " minutos";
+         } else {
+            minutosPorExtenso = dezena[minutoDezenaConvertidoNumero] + " e " + unidade[minutoUnidadeConvertidoNumero]
+                  + " minutos";
+         }
+
+      }
+
+      return minutosPorExtenso;
+
+   }
+
+   public static String segundosPorExtenso(String segundos) {
+
+      String segundosPorExtenso = "";
+
+      String[] dezena = { "", "", "vinte", "trinta", "quarenta", "cinquenta" };
+      String[] unidade = { "", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze",
+            "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove" };
+
+      int segundosConvertidosNumero = Integer.parseInt(segundos);
+
+      if (segundosConvertidosNumero >= 60) {
+         throw new Error("Os segundos são inválidos!");
+      }
+
+      if (segundosConvertidosNumero == 0) {
+         return "";
+      }
+
+      if (segundosConvertidosNumero == 1) {
+         segundosPorExtenso = unidade[segundosConvertidosNumero] + " segundo";
+      } else if (segundosConvertidosNumero < 20) {
+         segundosPorExtenso = unidade[segundosConvertidosNumero] + " segundos";
+      } else {
+         String segundosDezena = segundos.substring(0, 1);
+         String segundosUnidade = segundos.substring(1, 2);
+
+         int segundosDezenaConvertidosNumero = Integer.parseInt(segundosDezena);
+         int segundosUnidadeConvertidosNumero = Integer.parseInt(segundosUnidade);
+
+         if (segundosUnidadeConvertidosNumero == 0) {
+            segundosPorExtenso = dezena[segundosDezenaConvertidosNumero] + " segundos";
+         } else {
+            segundosPorExtenso = dezena[segundosDezenaConvertidosNumero] + " e "
+                  + unidade[segundosUnidadeConvertidosNumero] + " segundos";
+         }
+
+      }
+
+      return segundosPorExtenso;
+
+   }
 }
