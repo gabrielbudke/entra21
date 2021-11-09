919553940df755ec10c7c343f1d651f1e9c5ba28
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-11-09 20:18:10 +0000

    :recycle: Exercitando lógica de programação e Java

---
+++ src/br/com/entra21/exemplos/getterAndSetter/src/Brinquedo.java
@@ -0,0 +1,15 @@
+package br.com.entra21.exemplos.getterAndSetter.src;
+
+public class Brinquedo {
+   
+   private String nome;
+
+   public String getNome() {
+      return nome;
+   }
+
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+   
+}
---
+++ src/br/com/entra21/exemplos/getterAndSetter/test/BrinquedoTeste.java
@@ -0,0 +1,10 @@
+package br.com.entra21.exemplos.getterAndSetter.test;
+
+import org.junit.Test;
+
+public class BrinquedoTeste {
+   
+   @Test
+   public void testaSetNome() {}
+   
+}
