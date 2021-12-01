69ce70eaf24b622404f36e194f2af31802c8df35
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2021-12-01 03:36:13 +0000

    :sparkles: Exercitanprogramação Java

--- src/br/com/entra21/exercicios/lista08/GetterAndSetter/src/exercicios/filme/Filme.java
+++ src/br/com/entra21/exercicios/lista08/GetterAndSetter/src/exercicios/filme/Filme.java
@@ -1,7 +1,8 @@
 public class Filme {
 
-   private String titulo, tituloOriginal;
-
+   private String titulo, tituloOriginal, genero, diretor;
+   private int anoLancamento;
+   private double valorOrcamento, valorFaturamento;
 
    public String getTitulo() {
       return titulo;
@@ -18,5 +19,49 @@ public class Filme {
    public void setTituloOriginal(String tituloOriginal) {
       this.tituloOriginal = tituloOriginal;
    }
+
+   public String getGenero() {
+      return genero;
+   }
+
+   public void setGenero(String genero) {
+      this.genero = genero;
+   }
+
+   public String getDiretor() {
+      return diretor;
+   }
+
+   public void setDiretor(String diretor) {
+      this.diretor = diretor;
+   }
+
+   public int getAnoLancamento() {
+      return anoLancamento;
+   }
+
+   public void setAnoLancamento(int anoLancamento) {
+      this.anoLancamento = anoLancamento;
+   }
+
+   public double getValorOrcamento() {
+      return valorOrcamento;
+   }
+
+   public void setValorOrcamento(double valorOrcamento) {
+      this.valorOrcamento = valorOrcamento;
+   }
+
+   public double getValorFaturamento() {
+      return valorFaturamento;
+   }
+
+   public void setValorFaturamento(double valorFaturamento) {
+      this.valorFaturamento = valorFaturamento;
+   }
+
+   
+
+   
 
 }
--- src/br/com/entra21/exercicios/lista08/GetterAndSetter/src/exercicios/filme/FilmeTest.java
+++ src/br/com/entra21/exercicios/lista08/GetterAndSetter/src/exercicios/filme/FilmeTest.java
@@ -1,21 +1,57 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FilmeTest {
 
    @Test
-   public void definirEObteTitulo() {
+   public void testDefinirEObteTitulo() {
       Filme filme = new Filme();
       filme.setTitulo("Sociedade dos Poetas Mortos");
       assertEquals("Sociedade dos Poetas Mortos", filme.getTitulo());
    }
 
-   public void definirEObterTituloOriginal() {
+   @Test
+   public void testDefinirEObterTituloOriginal() {
       Filme filme = new Filme();
       filme.setTituloOriginal("Dead Society Poets");
       assertEquals("Dead Society Poets", filme.getTituloOriginal());
+   }
 
+   @Test
+   public void testDefinirEObterGenero() {
+      Filme filme = new Filme();
+      filme.setGenero("Drama");
+      assertEquals("Drama", filme.getGenero());
+   }
+
+   @Test
+   public void testDefinirEObterDiretor() {
+      Filme filme = new Filme();
+      filme.setDiretor("Peter Weir");
+      assertEquals("Peter Weir", filme.getDiretor());
+   }
+
+   @Test
+   public void testDefinirEObterAnoLancamento() {
+      Filme filme = new Filme();
+      filme.setAnoLancamento(1998);
+      assertEquals(1998, filme.getAnoLancamento());
+   }
+
+   @Test
+   public void testDefinirEObterValorOrcamento() {
+      Filme filme = new Filme();
+      filme.setValorOrcamento(16400000.00);
+      assertEquals(16400000.00, filme.getValorOrcamento(), 0);
+   }
+
+   @Test
+   public void testDefinirEObterValorFaturamento() {
+      Filme filme = new Filme();
+      filme.setValorFaturamento(235860116.00);
+      assertEquals(235860116, filme.getValorFaturamento(), 0);
    }
 
 }
